f6a97ae517e57aa1b7c1caafbfaed27f8c0a27c1
mike8317 <109864204+mike8317@users.noreply.github.com>
2022-08-14 23:07:33 +0700

    ВВод пользователем имени

---
+++ ExamplePrivetsnvie/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша");
+
+
+Console.WriteLine("Ура это Маша");
+   
+
+
+    Console.Write("Привет,");
+    Console.WriteLine(username);
